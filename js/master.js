5915b2e87cfbc6fb86a79fb756485dbd7d231dff
Md Enamul Haque <eng.enamulcse@gmail.com>
2020-10-06 00:48:56 +0600

    complete modal image view

--- js/master.js
+++ js/master.js
@@ -2,7 +2,6 @@ var scree_height = screen.height;
 
 
 var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
@@ -25,3 +24,46 @@ span.onclick = function() {
 //     modal.style.display = "none";
 //   }
 // }
+
+
+
+// Slider section
+var sliderimg = document.querySelectorAll(".slider_image");
+var left_btn = document.querySelector(".left_btn");
+var right_btn = document.querySelector(".right_btn");
+var current = 0;
+function reset(){
+  for(var i=0; i<sliderimg.length;i++){
+    sliderimg[i].style.display = "none";
+  }
+}
+
+function start(){
+  reset();
+  sliderimg[0].style.display = "block";
+}
+start();
+
+function leftSlide(){
+  reset();
+  if(current===0){
+    current=sliderimg.length;
+  }
+  sliderimg[current-1].style.display = "block";
+  current--;
+}
+function rightSlide(){
+  reset();
+  if(current===sliderimg.length-1){
+    current= -1;
+  }
+  sliderimg[current+1].style.display = "block";
+  current++;
+}
+
+left_btn.addEventListener("click", function(){
+   leftSlide();
+});
+right_btn.addEventListener("click", function(){
+   rightSlide();
+});
